44051a58a85907e32bc6405a18897abf8fc51059
Leland Richardson <leland.m.richardson@gmail.com>
2017-02-01 14:31:10 -0800

    Fix lint errors

---
+++ example/ExamplePicker.js
@@ -0,0 +1,39 @@
+/* eslint-disable global-require */
+import React, { PropTypes } from 'react';
+import {
+  Picker,
+  Platform,
+} from 'react-native';
+
+const propTypes = {
+  example: PropTypes.any,
+  onChange: PropTypes.func,
+  examples: PropTypes.any,
+};
+
+export default class ExamplePicker extends React.Component {
+  render() {
+    return (
+      <Picker
+        selectedValue={this.props.example}
+        onValueChange={this.props.onChange}
+        style={{
+          marginBottom: Platform.select({
+            ios: -30,
+            android: 0,
+          }),
+        }}
+      >
+        {Object.keys(this.props.examples).map(name => this.props.examples[name]).map(ex => (
+          <Picker.Item
+            key={ex.name}
+            label={ex.name}
+            value={ex.name}
+          />
+        ))}
+      </Picker>
+    );
+  }
+}
+
+ExamplePicker.propTypes = propTypes;
