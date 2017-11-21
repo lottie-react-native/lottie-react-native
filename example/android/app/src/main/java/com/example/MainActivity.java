e089dfbeb412cd5aa9e7d0198ee2ea41f62e0cf8
Gabriel Peal <gabriel.peal@airbnb.com>
2017-11-21 10:09:45 -0800

    [Android] Enable double tap d for dev menu

--- example/android/app/src/main/java/com/example/MainActivity.java
+++ example/android/app/src/main/java/com/example/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example;
 
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+
 import com.facebook.react.ReactActivity;
 
 public class MainActivity extends ReactActivity {
+
+    private final DoubleTapRecognizer doubleTapMenuRecognizer = new DoubleTapRecognizer (KeyEvent.KEYCODE_D);
 
     /**
      * Returns the name of the main component registered from JavaScript.
@@ -11,5 +17,21 @@ public class MainActivity extends ReactActivity {
     @Override
     protected String getMainComponentName() {
         return "example";
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getReactInstanceManager().getDevSupportManager().setDevSupportEnabled(true);
+    }
+
+    @Override
+    public boolean onKeyUp(int keyCode, KeyEvent event) {
+        View currentFocus = getCurrentFocus();
+        if (doubleTapMenuRecognizer.didDoubleTap(keyCode, currentFocus)) {
+            getReactInstanceManager().getDevSupportManager().showDevOptionsDialog();
+            return true;
+        }
+        return super.onKeyUp(keyCode, event);
     }
 }
