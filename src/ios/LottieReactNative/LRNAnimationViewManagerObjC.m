a2700e8b1f020f2c95e412055db19cad96609bbe
Leon Omelan <rozpierog@gmail.com>
2019-06-07 17:58:05 +0200

    Revert "Revert "[iOS] forgot to add  color filter to .m""
    This reverts commit 6cb3766b774c7bb72682adc793d58e94cd30713d.

--- src/ios/LottieReactNative/LRNAnimationViewManagerObjC.m
+++ src/ios/LottieReactNative/LRNAnimationViewManagerObjC.m
@@ -15,6 +15,7 @@ RCT_EXPORT_VIEW_PROPERTY(sourceName, NSString);
 RCT_EXPORT_VIEW_PROPERTY(progress, CGFloat);
 RCT_EXPORT_VIEW_PROPERTY(loop, BOOL);
 RCT_EXPORT_VIEW_PROPERTY(speed, CGFloat);
+RCT_EXPORT_VIEW_PROPERTY(color, NSString);
 RCT_EXPORT_VIEW_PROPERTY(onAnimationFinish, RCTBubblingEventBlock);
 
 RCT_EXTERN_METHOD(play:(nonnull NSNumber *)reactTag fromFrame:(nonnull NSNumber *) startFrame toFrame:(nonnull NSNumber *) endFrame )
