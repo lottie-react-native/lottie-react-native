d66efe6c3da878c90c8e861b733178a05a50bff1
Andrew Tremblay <andrew.tremblay-rp@peartherapeutics.com>
2019-06-27 10:30:35 -0400

    addressing prettier failing  checks

--- src/js/LottieView.js
+++ src/js/LottieView.js
@@ -82,14 +82,14 @@ const viewConfig = {
   },
 };
 
-const safeGetViewManagerConfig = (moduleName) => {
+const safeGetViewManagerConfig = moduleName => {
   if (UIManager.getViewManagerConfig) {
     // RN >= 0.58
-    return UIManager.getViewManagerConfig(moduleName)
+    return UIManager.getViewManagerConfig(moduleName);
   }
   // RN < 0.58
   return UIManager[moduleName];
-}
+};
 
 class LottieView extends React.Component {
   constructor(props) {
