4a56672f728b3423ed3e97c6bdfeef2e54509743
radex <radexpl@gmail.com>
2019-06-28 17:51:09 +0200

    Clean up Swift

--- src/ios/LottieReactNative/ContainerView.swift
+++ src/ios/LottieReactNative/ContainerView.swift
@@ -4,134 +4,97 @@ import React
 
 import Lottie
 
-class ContainerView:RCTView {
-    
-    private var speed:CGFloat = 0.0
-    private var progress:CGFloat = 0.0
-    private var loop:LottieLoopMode = .playOnce
-    private var sourceJson:String = ""
-    private var resizeMode:String = ""
-    private var sourceName:String = ""
-    @objc var onAnimationFinish:RCTBubblingEventBlock?
-    var animationView:AnimationView?
+class ContainerView: RCTView {
+    private var speed: CGFloat = 0.0
+    private var progress: CGFloat = 0.0
+    private var loop: LottieLoopMode = .playOnce
+    private var sourceJson: String = ""
+    private var resizeMode: String = ""
+    private var sourceName: String = ""
+    @objc var onAnimationFinish: RCTBubblingEventBlock?
+    var animationView: AnimationView?
 
-    
-    @objc func setSpeed(_ speed: CGFloat) {
-        self.speed = speed
+    @objc func setSpeed(_ newSpeed: CGFloat) {
+        speed = newSpeed
     }
     
-    @objc func setProgress(_ progress: CGFloat) {
-        self.progress = progress
-        if (self.animationView != nil) {
-            self.animationView?.currentProgress =  progress;
-        }
+    @objc func setProgress(_ newProgress: CGFloat) {
+        progress = newProgress
+        animationView?.currentProgress = progress
     }
     
     override func reactSetFrame(_ frame: CGRect) {
         super.reactSetFrame(frame)
-        if (self.animationView != nil) {
-            self.animationView?.reactSetFrame(frame);
-        }
+        animationView?.reactSetFrame(frame)
     }
     
-    @objc func setLoop(_ loop: Bool) {
-        if (loop) {
-            self.loop = .loop
-        } else {
-            self.loop = .playOnce
-        }
-        
-        if (self.animationView != nil) {
-            self.animationView?.loopMode =  self.loop;
-        }
+    @objc func setLoop(_ isLooping: Bool) {
+        loop = isLooping ? .loop : .playOnce
+        animationView?.loopMode = loop
     }
     
-    @objc func setSourceJson(_ sourceJson: String) {
-        self.sourceJson = sourceJson
-        let starAnimationView = AnimationView()
-        guard let data: Data = sourceJson.data(using: String.Encoding.utf8) else {
-            if (RCT_DEV == 1) {
-                print("Unable to create Data from the json string")
-            }
-            return
-        }
-        
-        do {
-            let animation = try JSONDecoder().decode(Animation.self, from: data)
-            starAnimationView.animation = animation
-            self.replaceAnimationView(next: starAnimationView)
-        } catch {
+    @objc func setSourceJson(_ newSourceJson: String) {
+        sourceJson = newSourceJson
+
+        guard let data = sourceJson.data(using: String.Encoding.utf8),
+        let animation = try? JSONDecoder().decode(Animation.self, from: data) else {
             if (RCT_DEV == 1) {
                 print("Unable to create the lottie animation obeject from the JSON source")
             }
+            return
         }
+
+        let starAnimationView = AnimationView()
+        starAnimationView.animation = animation
+        replaceAnimationView(next: starAnimationView)
     }
     
-    @objc func setSourceName(_ sourceName: String) {
-        self.sourceName = sourceName
+    @objc func setSourceName(_ newSourceName: String) {
+        sourceName = newSourceName
     }
     
     @objc func setResizeMode(_ resizeMode: String) {
-        if ( resizeMode == "cover" ) {
+        switch (resizeMode) {
+        case "cover":
             animationView?.contentMode = .scaleAspectFill
-        } else if ( resizeMode == "contain" ) {
+        case "contain":
             animationView?.contentMode = .scaleAspectFit
-        } else if ( resizeMode == "center" ) {
+        case "center":
             animationView?.contentMode = .center
+        default: break
         }
     }
     
     
-    func play(fromFrame: AnimationFrameTime? = nil,
-                     toFrame: AnimationFrameTime,
-                     completion: LottieCompletionBlock? = nil) {
-        if (self.animationView != nil) {
-            self.animationView?.play(fromFrame: fromFrame, toFrame: toFrame, loopMode: self.loop, completion: completion);
-        }
+    func play(fromFrame: AnimationFrameTime? = nil, toFrame: AnimationFrameTime, completion: LottieCompletionBlock? = nil) {
+        animationView?.play(fromFrame: fromFrame, toFrame: toFrame, loopMode: self.loop, completion: completion);
     }
     
-    func play( completion: LottieCompletionBlock? = nil) {
-        if (self.animationView != nil) {
-             if (completion != nil) {
-                self.animationView?.play(completion: completion);
-             } else {
-                 self.animationView?.play()
-            }
-        }
-    }
-    
-    func play() {
-        if (self.animationView != nil) {
-           self.animationView?.play()
-        }
+    func play(completion: LottieCompletionBlock? = nil) {
+        animationView?.play(completion: completion)
     }
     
     func reset() {
-        if (self.animationView != nil) {
-            self.animationView?.currentProgress = 0;
-            self.animationView?.pause()
-        }
+        animationView?.currentProgress = 0;
+        animationView?.pause()
     }
     
     // MARK: Private
     
-    func replaceAnimationView(next:AnimationView) {
-        var contentMode:UIView.ContentMode = .scaleAspectFit
-        if (self.animationView != nil) {
-            contentMode = self.animationView?.contentMode ?? .scaleAspectFit
-            self.animationView?.removeFromSuperview()
-        }
-        
-        self.animationView = next
-        self.addSubview(next)
-        self.animationView?.contentMode = contentMode
-        self.animationView?.reactSetFrame(self.frame)
-        self.applyProperties()
+    func replaceAnimationView(next: AnimationView) {
+        animationView?.removeFromSuperview()
+
+        let contentMode = animationView?.contentMode ?? .scaleAspectFit
+        animationView = next
+        addSubview(next)
+        animationView?.contentMode = contentMode
+        animationView?.reactSetFrame(frame)
+        applyProperties()
     }
     
     func applyProperties() {
-        self.animationView?.currentProgress = self.progress;
-        self.animationView?.animationSpeed = self.speed;
-        self.animationView?.loopMode = self.loop;
+        animationView?.currentProgress = progress
+        animationView?.animationSpeed = speed
+        animationView?.loopMode = loop
     }
 }
--- src/ios/LottieReactNative/LRNAnimationViewManagerObjC.m
+++ src/ios/LottieReactNative/LRNAnimationViewManagerObjC.m
@@ -10,9 +10,9 @@ RCT_EXPORT_VIEW_PROPERTY(loop, BOOL);
 RCT_EXPORT_VIEW_PROPERTY(speed, CGFloat);
 RCT_EXPORT_VIEW_PROPERTY(onAnimationFinish, RCTBubblingEventBlock);
 
-RCT_EXTERN_METHOD(play:(nonnull NSNumber *)reactTag fromFrame:(nonnull NSNumber *) startFrame toFrame:(nonnull NSNumber *) endFrame )
+RCT_EXTERN_METHOD(play:(nonnull NSNumber *)reactTag fromFrame:(nonnull NSNumber *) startFrame toFrame:(nonnull NSNumber *) endFrame);
 
-RCT_EXTERN_METHOD(reset:(nonnull NSNumber *)reactTag)
+RCT_EXTERN_METHOD(reset:(nonnull NSNumber *)reactTag);
 
 @end
 
