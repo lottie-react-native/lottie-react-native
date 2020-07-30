d886ebb1f9c5c2e8256fe38e3ecdd65dffb35506
Simone Carnio <simone.carnio@foolix.com>
2020-07-30 16:45:42 +0200

    fix alpha on colorFilters on iOS

--- src/ios/LottieReactNative/ContainerView.swift
+++ src/ios/LottieReactNative/ContainerView.swift
@@ -94,21 +94,22 @@ class ContainerView: RCTView {
         }
 
         if ((cString.count) == 0) {
-            return UIColor.red
+            return UIColor.white
         }
 
-        if ((cString.count) != 6) {
-            return UIColor.green
+        if ((cString.count) == 6) {
+            cString  = cString + "FF"
         }
 
-        var rgbValue:UInt32 = 0
-        Scanner(string: cString).scanHexInt32(&rgbValue)
+        var rgbValue:UInt64 = 0
+        Scanner(string: cString).scanHexInt64(&rgbValue)
 
         return UIColor(
-            red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
-            green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
-            blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
-            alpha: CGFloat(1.0)
+            red: CGFloat((rgbValue & 0xFF000000) >> 24) / 255.0,
+            green: CGFloat((rgbValue & 0x00FF0000) >> 16) / 255.0,
+            blue: CGFloat((rgbValue & 0x0000FF00) >> 8) / 255.0,
+            alpha: CGFloat(rgbValue & 0x000000FF) / 255.0
+            
         )
     }
 
