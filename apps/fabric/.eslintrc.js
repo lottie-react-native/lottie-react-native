5e1673d19f34637859b361c2328b585f5ff99120
Alexander Eggers <7427545+AlexanderEggers@users.noreply.github.com>
2022-10-28 16:10:04 +1100

    feat: tweaking fabric example project

---
+++ apps/fabric/.eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  root: true,
+  extends: '@react-native-community',
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      rules: {
+        '@typescript-eslint/no-shadow': ['error'],
+        'no-shadow': 'off',
+        'no-undef': 'off',
+      },
+    },
+  ],
+};
--- apps/fabric/ios/exampleTests/exampleTests.m
+++ apps/fabric/ios/exampleTests/exampleTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface RnDiffAppTests : XCTestCase
+@interface exampleTests : XCTestCase
 
 @end
 
-@implementation RnDiffAppTests
+@implementation exampleTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
