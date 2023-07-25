9dfd4f53efa7bed1f6143fc46ae015263e5063dc
Matin Zadeh Dolatabad <zadehdolatabad@gmail.com>
2023-07-25 20:15:54 +0200

    fix(ios): animation bounding frame with parent view (#1062)
    * fix(ios): animation bounding frame with parent view
    
    - cleanup some code for readability
    
    * fix: formatting

--- packages/core/ios/LottieReactNative/ContainerView.swift
+++ packages/core/ios/LottieReactNative/ContainerView.swift
@@ -34,7 +34,7 @@ class ContainerView: RCTView {
         };
     }
     
-    #if !(os(OSX))
+#if !(os(OSX))
     override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
         super.traitCollectionDidChange(previousTraitCollection)
         if #available(iOS 13.0, tvOS 13.0, *) {
@@ -43,11 +43,11 @@ class ContainerView: RCTView {
             }
         }
     }
-    #endif
-
+#endif
+    
     @objc func setSpeed(_ newSpeed: CGFloat) {
         speed = newSpeed
-
+        
         if (newSpeed != 0.0) {
             animationView?.animationSpeed = newSpeed
             if (!(animationView?.isAnimationPlaying ?? true)) {
@@ -57,12 +57,12 @@ class ContainerView: RCTView {
             animationView?.pause()
         }
     }
-
+    
     @objc func setProgress(_ newProgress: CGFloat) {
         progress = newProgress
         animationView?.currentProgress = progress
     }
-
+    
     @objc func setLoop(_ isLooping: Bool) {
         loop = isLooping ? .loop : .playOnce
         animationView?.loopMode = loop
@@ -70,9 +70,7 @@ class ContainerView: RCTView {
     
     @objc func setAutoPlay(_ autoPlay: Bool) {
         self.autoPlay = autoPlay
-        if(autoPlay && animationView?.isAnimationPlaying == false) {
-            self.play()
-        }
+        playIfNeeded()
     }
     
     @objc func setTextFiltersIOS(_ newTextFilters: [NSDictionary]) {
@@ -92,13 +90,13 @@ class ContainerView: RCTView {
             replaceAnimationView(next: nextAnimationView)
         }
     }
-
-    func getLottieConfiguration() -> LottieConfiguration {
+    
+    var lottieConfiguration: LottieConfiguration {
         return LottieConfiguration(
             renderingEngine: renderMode
         )
     }
-       
+    
     @objc func setRenderMode(_ newRenderMode: String) {
         switch newRenderMode {
         case "SOFTWARE":
@@ -119,15 +117,17 @@ class ContainerView: RCTView {
             }
             renderMode = .automatic
         }
+        
         if (animationView != nil) {
             let nextAnimationView = LottieAnimationView(
                 animation: animationView?.animation,
-                configuration: getLottieConfiguration()
+                configuration: lottieConfiguration
             )
+            
             replaceAnimationView(next: nextAnimationView)
         }
     }
-
+    
     @objc func setSourceURL(_ newSourceURLString: String) {
         var url = URL(string: newSourceURLString)
         
@@ -135,67 +135,47 @@ class ContainerView: RCTView {
             // interpret raw URL paths as relative to the resource bundle
             url = URL(fileURLWithPath: newSourceURLString, relativeTo: Bundle.main.resourceURL)
         }
+        
         guard let url else {
             return
         }
         
-        DispatchQueue.global(qos: .default).async {
-            do {
-                let sourceJson = try String(contentsOf: url)
-                guard let data = sourceJson.data(using: String.Encoding.utf8),
-                let animation = try? JSONDecoder().decode(LottieAnimation.self, from: data) else {
-                    if (RCT_DEBUG == 1) {
-                        print("Unable to decode the lottie animation object from the fetched URL source")
-                    }
-                    return
-                }
-                DispatchQueue.main.async { [weak self] in
-                    guard let self else { return }
-                    let nextAnimationView = LottieAnimationView(
-                        animation: animation,
-                        configuration: self.getLottieConfiguration()
-                    )
-                    self.replaceAnimationView(next: nextAnimationView)
-                }
-            } catch {
-                if (RCT_DEBUG == 1) {
-                    print("Unable to load the lottie animation URL")
-                }
-            }
-        }
+        self.fetchRemoteAnimation(from: url)
     }
-
+    
     @objc func setSourceJson(_ newSourceJson: String) {
         sourceJson = newSourceJson
-
+        
         guard let data = sourceJson.data(using: String.Encoding.utf8),
-        let animation = try? JSONDecoder().decode(LottieAnimation.self, from: data) else {
+              let animation = try? JSONDecoder().decode(LottieAnimation.self, from: data) else {
             if (RCT_DEBUG == 1) {
                 print("Unable to create the lottie animation object from the JSON source")
             }
             return
         }
-
+        
         let nextAnimationView = LottieAnimationView(
             animation: animation,
-            configuration: getLottieConfiguration()
+            configuration: lottieConfiguration
         )
+        
         replaceAnimationView(next: nextAnimationView)
     }
-
+    
     @objc func setSourceName(_ newSourceName: String) {
         if (newSourceName == sourceName) {
-          return
+            return
         }
         sourceName = newSourceName
-
+        
         let nextAnimationView = LottieAnimationView(
             name: sourceName,
-            configuration: getLottieConfiguration()
+            configuration: lottieConfiguration
         )
+        
         replaceAnimationView(next: nextAnimationView)
     }
-
+    
     @objc func setResizeMode(_ resizeMode: String) {
         switch (resizeMode) {
         case "cover":
@@ -207,12 +187,12 @@ class ContainerView: RCTView {
         default: break
         }
     }
-
+    
     @objc func setColorFilters(_ newColorFilters: [NSDictionary]) {
         colorFilters = newColorFilters
         applyColorProperties()
     }
-
+    
     // There is no Nullable CGFloat in Objective-C, so this function uses a Nullable NSNumber and converts it later
     @objc(playFromFrame:toFrame:)
     func objcCompatiblePlay(fromFrame: NSNumber? = nil, toFrame: AnimationFrameTime) {
@@ -223,53 +203,54 @@ class ContainerView: RCTView {
     func play(fromFrame: AnimationFrameTime? = nil, toFrame: AnimationFrameTime) {
         animationView?.play(fromFrame: fromFrame, toFrame: toFrame, loopMode: self.loop, completion: completionCallback);
     }
-
+    
     @objc func play() {
         animationView?.play(completion: completionCallback)
     }
-
+    
     @objc func reset() {
         animationView?.currentProgress = 0;
         animationView?.pause()
     }
-
+    
     @objc func pause() {
         animationView?.pause()
     }
-
+    
     @objc func resume() {
         play()
     }
-
+    
     // The animation view is a child of the RCTView, so if the bounds ever change, add those changes to the animation view as well
     override var bounds: CGRect {
         didSet {
-            animationView?.reactSetFrame(self.bounds)
+            animationView?.frame = self.bounds
         }
     }
     
     // MARK: Private
     func replaceAnimationView(next: LottieAnimationView) {
-        animationView?.removeFromSuperview()
-
+        super.removeReactSubview(animationView)
+        
         let contentMode = animationView?.contentMode ?? .scaleAspectFit
+        
         animationView = next
-        addSubview(next)
+        
         animationView?.contentMode = contentMode
-        animationView?.reactSetFrame(self.bounds)
         animationView?.backgroundBehavior = .pauseAndRestore
         animationView?.animationSpeed = speed
         animationView?.loopMode = loop
-        applyColorProperties()
+        animationView?.frame = self.bounds
         
-        if (self.autoPlay && animationView?.isAnimationPlaying == false) {
-            self.play()
-        }
+        addSubview(next)
+        
+        applyColorProperties()
+        playIfNeeded()
     }
     
     func applyColorProperties() {
         guard let animationView = animationView else { return }
-
+        
         if (colorFilters.count > 0) {
             for filter in colorFilters {
                 let keypath: String = "\(filter.value(forKey: "keypath") as! String).**.Color"
@@ -278,5 +259,49 @@ class ContainerView: RCTView {
                 animationView.setValueProvider(colorFilterValueProvider, keypath: fillKeypath)
             }
         }
+    }
+    
+    func playIfNeeded() {
+        if(autoPlay && animationView?.isAnimationPlaying == false) {
+            self.play()
+        }
+    }
+    
+    func fetchRemoteAnimation(from url: URL) {
+        URLSession.shared.dataTask(with: url) { [weak self] data, response, error in
+            guard let self = self else { return }
+            
+            if let error = error {
+                if RCT_DEBUG == 1 {
+                    print("Unable to fetch the Lottie animation from the URL: \(error)")
+                }
+                return
+            }
+            
+            guard let data = data else {
+                if RCT_DEBUG == 1 {
+                    print("No data received for the Lottie animation from the URL.")
+                }
+                return
+            }
+            
+            do {
+                let animation = try JSONDecoder().decode(LottieAnimation.self, from: data)
+                
+                DispatchQueue.main.async { [weak self] in
+                    guard let self = self else { return }
+                    
+                    let nextAnimationView = LottieAnimationView(
+                        animation: animation,
+                        configuration: self.lottieConfiguration
+                    )
+                    self.replaceAnimationView(next: nextAnimationView)
+                }
+            } catch {
+                if RCT_DEBUG == 1 {
+                    print("Unable to decode the Lottie animation object from the fetched URL source: \(error)")
+                }
+            }
+        }.resume()
     }
 }
