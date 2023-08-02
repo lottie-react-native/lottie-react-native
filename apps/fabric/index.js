7c5dbc639ed228f7d0bb666f2f0122d3f27c45a6
matinzd <zadehdolatabad@gmail.com>
2023-08-02 12:11:27 +0200

    fix: apply suggestions

--- apps/fabric/index.js
+++ apps/fabric/index.js
@@ -1,7 +1,5 @@
 import {AppRegistry} from 'react-native';
 import App from './src/App';
 import {name as appName} from './app.json';
-import {DotLottieExample} from './src/DotLottieExample';
 
-// AppRegistry.registerComponent(appName, () => App);
-AppRegistry.registerComponent(appName, () => DotLottieExample);
+AppRegistry.registerComponent(appName, () => App);
