486e00c0b5b74e98a0bc77f401162d605883037d
Łukasz Kosmaty <kosmatylukasz@gmail.com>
2024-10-24 14:36:22 +0200

    chore: Remove legacy version check in `react-native.config.js`

--- packages/core/react-native.config.js
+++ packages/core/react-native.config.js
@@ -1,22 +1,10 @@
-let supportsCodegenConfig = false;
-try {
-  const rnCliAndroidVersion =
-    require('@react-native-community/cli-platform-android/package.json').version;
-  const [major] = rnCliAndroidVersion.split('.');
-  supportsCodegenConfig = major >= 9;
-} catch (e) {
-  // ignore
-}
-
 module.exports = {
   dependency: {
     platforms: {
-      android: supportsCodegenConfig
-        ? {
-            libraryName: 'lottiereactnative',
-            componentDescriptors: ['LottieAnimationViewComponentDescriptor'],
-          }
-        : {},
+      android: {
+        libraryName: 'lottiereactnative',
+        componentDescriptors: ['LottieAnimationViewComponentDescriptor'],
+      },
       macos: null,
       windows: null,
     },
