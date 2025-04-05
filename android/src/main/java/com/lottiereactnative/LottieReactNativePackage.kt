3609306464142c12f2bac13c24abd16ca1a281f1
Parsa Nasirimehr <p.nasirimehr@gmail.com>
2025-04-05 17:59:46 +0200

    fix: missing lottie view not being exposed on Android

--- android/src/main/java/com/lottiereactnative/LottieReactNativePackage.kt
+++ android/src/main/java/com/lottiereactnative/LottieReactNativePackage.kt
@@ -1,11 +1,13 @@
 package com.lottiereactnative
 
-import com.facebook.react.TurboReactPackage
+import com.facebook.react.BaseReactPackage
 import com.facebook.react.bridge.NativeModule
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.module.model.ReactModuleInfoProvider
+import com.facebook.react.uimanager.ViewManager
+import com.margelo.nitro.lottiereactnative.views.HybridLottieAnimationViewManager
 
-class LottieReactNativePackage : TurboReactPackage() {
+class LottieReactNativePackage : BaseReactPackage() {
     override fun getModule(name: String, reactContext: ReactApplicationContext): NativeModule? {
         return null
     }
@@ -13,6 +15,10 @@ class LottieReactNativePackage : TurboReactPackage() {
     override fun getReactModuleInfoProvider(): ReactModuleInfoProvider {
         return ReactModuleInfoProvider { HashMap() }
     }
+
+  override fun createViewManagers(reactContext: ReactApplicationContext): List<ViewManager<*, *>> {
+    return listOf(HybridLottieAnimationViewManager())
+  }
 
     companion object {
         init {
